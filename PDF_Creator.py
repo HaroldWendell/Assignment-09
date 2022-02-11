7e597e98f5aae833c25aca1abd17864ccbc24ee2
HaroldWendell <haroldwendellebarredo@iskolarngbayan.pup.edu.ph>
2022-02-11 19:47:30 +0800

    Now, I was to create a PDF and convert the basic data in the JSON file to PDF format.

--- PDF_Creator.py
+++ PDF_Creator.py
@@ -9,3 +9,17 @@ pdf = FPDF()
 pdf = FPDF(orientation='P', unit='mm', format='Letter')
 pdf.add_page()
 pdf.rect(5.0, 5.0, 206.0, 269.0)
+
+with open('Resume_data.json') as data:
+    info = json.load(data)
+
+for line in info:
+    pdf.set_font("Arial", 'B', size=20)
+    pdf.cell(80, 30, line['Name'], ln = 1, align = "L")
+    pdf.set_font("Arial", size=14)
+    pdf.cell(80, -15, txt = "Gender: " + line['Gender'], ln = 1, align ="L")
+    pdf.cell(80, 28, txt = "Age: " + line['Age'], ln = 1, align ="L")
+    pdf.cell(80, -15, txt = "Contact No. " + line['Contact No.'], ln = 1, align ="L")
+    pdf.cell(80, 28, txt = "Email: " + line['Email'], ln = 1, align ="L")
+
+pdf.output("BARREDO_HAROLD_WENDELL.pdf")
